f0d302e21624e24e8bf461c3230f4c7a6b9fe55c
BlommePieter <pieter.blomme@student.howest.be>
2023-01-23 13:24:05 +0100

    update: meer in game klasse

--- JachtSeizoen.cs
+++ JachtSeizoen.cs
@@ -2,250 +2,250 @@ namespace MCT.Function;
 
 public class JachtSeizoen
 {
-	[FunctionName("GetGames")]
-	public static async Task<IActionResult> GetGames(
-		[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "games")] HttpRequest req,
-		ILogger log)
-	{
-		try
-		{
-			var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
+    [FunctionName("GetGames")]
+    public static async Task<IActionResult> GetGames(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "games")] HttpRequest req,
+        ILogger log)
+    {
+        try
+        {
+            var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
 
-			CosmosClientOptions options = new CosmosClientOptions()
-			{
-				ConnectionMode = ConnectionMode.Gateway
-			};
+            CosmosClientOptions options = new CosmosClientOptions()
+            {
+                ConnectionMode = ConnectionMode.Gateway
+            };
 
-			CosmosClient client = new CosmosClient(ConnectionString, options);
-			var container = client.GetContainer(General.COSMOS_DB_JACHTSEIZOEN, General.COSMOS_CONTAINER_GAMES);
+            CosmosClient client = new CosmosClient(ConnectionString, options);
+            var container = client.GetContainer(General.COSMOS_DB_JACHTSEIZOEN, General.COSMOS_CONTAINER_GAMES);
 
-			string sql = "SELECT * FROM c";
-			var iterator = container.GetItemQueryIterator<Game>(sql);
-			var results = new List<Game>();
+            string sql = "SELECT * FROM c";
+            var iterator = container.GetItemQueryIterator<Game>(sql);
+            var results = new List<Game>();
 
-			while (iterator.HasMoreResults)
-			{
-				var response = await iterator.ReadNextAsync();
-				results.AddRange(response.ToList());
-			}
+            while (iterator.HasMoreResults)
+            {
+                var response = await iterator.ReadNextAsync();
+                results.AddRange(response.ToList());
+            }
 
-			return new OkObjectResult(results);
-		}
-		catch (Exception ex)
-		{
-			log.LogError(ex.Message);
-			return new BadRequestObjectResult(ex.Message);
-		}
+            return new OkObjectResult(results);
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex.Message);
+            return new BadRequestObjectResult(ex.Message);
+        }
 
-	}
+    }
 
-	[FunctionName("AddGame")]
-	public static async Task<IActionResult> AddGame(
-		[HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "games")] HttpRequest req,
-		ILogger log)
-	{
-		try
-		{
-			// body uitlezen en omzetten naar Person object
-			var json = await new StreamReader(req.Body).ReadToEndAsync();
-			var task = JsonConvert.DeserializeObject<Game>(json);
+    [FunctionName("AddGame")]
+    public static async Task<IActionResult> AddGame(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "games")] HttpRequest req,
+        ILogger log)
+    {
+        try
+        {
+            // body uitlezen en omzetten naar Person object
+            var json = await new StreamReader(req.Body).ReadToEndAsync();
+            var task = JsonConvert.DeserializeObject<Game>(json);
 
-			// connectie maken met CosmosDb
-			var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
+            // connectie maken met CosmosDb
+            var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
 
-			CosmosClientOptions options = new CosmosClientOptions()
-			{
-				ConnectionMode = ConnectionMode.Gateway
-			};
+            CosmosClientOptions options = new CosmosClientOptions()
+            {
+                ConnectionMode = ConnectionMode.Gateway
+            };
 
-			CosmosClient client = new CosmosClient(ConnectionString, options);
-			var container = client.GetContainer(General.COSMOS_DB_JACHTSEIZOEN, General.COSMOS_CONTAINER_GAMES);
-			task.Id = Guid.NewGuid().ToString();
-			await container.CreateItemAsync(task, new PartitionKey(task.Id));
+            CosmosClient client = new CosmosClient(ConnectionString, options);
+            var container = client.GetContainer(General.COSMOS_DB_JACHTSEIZOEN, General.COSMOS_CONTAINER_GAMES);
+            task.Id = Guid.NewGuid().ToString();
+            await container.CreateItemAsync(task, new PartitionKey(task.Id));
 
-			return new OkObjectResult(task);
-		}
-		catch (Exception ex)
-		{
-			log.LogError(ex.Message);
-			return new BadRequestObjectResult(ex.Message);
-		}
+            return new OkObjectResult(task);
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex.Message);
+            return new BadRequestObjectResult(ex.Message);
+        }
 
-	}
+    }
 
-	[FunctionName("UpdateGame")]
-	public static async Task<IActionResult> UpdateGame(
-		[HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "games")] HttpRequest req,
-		ILogger log)
-	{
-		try
-		{
-			// body uitlezen en omzetten naar Person object
-			var json = await new StreamReader(req.Body).ReadToEndAsync();
-			var game = JsonConvert.DeserializeObject<Game>(json);
+    [FunctionName("UpdateGame")]
+    public static async Task<IActionResult> UpdateGame(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "games")] HttpRequest req,
+        ILogger log)
+    {
+        try
+        {
+            // body uitlezen en omzetten naar Person object
+            var json = await new StreamReader(req.Body).ReadToEndAsync();
+            var game = JsonConvert.DeserializeObject<Game>(json);
 
-			// connectie maken met CosmosDb
-			var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
+            // connectie maken met CosmosDb
+            var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
 
-			CosmosClientOptions options = new CosmosClientOptions()
-			{
-				ConnectionMode = ConnectionMode.Gateway
-			};
+            CosmosClientOptions options = new CosmosClientOptions()
+            {
+                ConnectionMode = ConnectionMode.Gateway
+            };
 
-			CosmosClient client = new CosmosClient(ConnectionString, options);
+            CosmosClient client = new CosmosClient(ConnectionString, options);
 
-			var container = client.GetContainer(General.COSMOS_DB_JACHTSEIZOEN, General.COSMOS_CONTAINER_GAMES);
-			await container.ReplaceItemAsync(game, game.Id);
+            var container = client.GetContainer(General.COSMOS_DB_JACHTSEIZOEN, General.COSMOS_CONTAINER_GAMES);
+            await container.ReplaceItemAsync(game, game.Id);
 
-			return new OkObjectResult(game);
-		}
-		catch (Exception ex)
-		{
-			log.LogError(ex.Message);
-			return new BadRequestObjectResult(ex.Message);
-		}
+            return new OkObjectResult(game);
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex.Message);
+            return new BadRequestObjectResult(ex.Message);
+        }
 
-	}
+    }
 
-	[FunctionName("GetGamefromGroep")]
-	public static async Task<IActionResult> GetGameFromgroep(
-		[HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "games/{groep}")] HttpRequest req,
-		string groep,
-		ILogger log)
-	{
-		try
-		{
-			var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
+    [FunctionName("GetGamefromGroep")]
+    public static async Task<IActionResult> GetGameFromgroep(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "games/{groep}")] HttpRequest req,
+        string groep,
+        ILogger log)
+    {
+        try
+        {
+            var ConnectionString = Environment.GetEnvironmentVariable("CosmosDb");
 
-			CosmosClientOptions options = new CosmosClientOptions()
-			{
-				ConnectionMode = ConnectionMode.Gateway
-			};
+            CosmosClientOptions options = new CosmosClientOptions()
+            {
+                ConnectionMode = ConnectionMode.Gateway
+            };
 
-			CosmosClient client = new CosmosClient(ConnectionString, options);
-			var container = client.GetContainer(General.COSMOS_DB_JACHTSEIZOEN, General.COSMOS_CONTAINER_GAMES);
+            CosmosClient client = new CosmosClient(ConnectionString, options);
+            var container = client.GetContainer(General.COSMOS_DB_JACHTSEIZOEN, General.COSMOS_CONTAINER_GAMES);
 
-			string sql = $"SELECT * FROM c WHERE c.groep = '{groep}'";
-			var iterator = container.GetItemQueryIterator<Game>(sql);
-			var results = new List<Game>();
+            string sql = $"SELECT * FROM c WHERE c.groep = '{groep}'";
+            var iterator = container.GetItemQueryIterator<Game>(sql);
+            var results = new List<Game>();
 
-			while (iterator.HasMoreResults)
-			{
-				var response = await iterator.ReadNextAsync();
-				results.AddRange(response.ToList());
-			}
+            while (iterator.HasMoreResults)
+            {
+                var response = await iterator.ReadNextAsync();
+                results.AddRange(response.ToList());
+            }
 
-			return new OkObjectResult(results);
-		}
-		catch (Exception ex)
-		{
-			log.LogError(ex.Message);
-			return new BadRequestObjectResult(ex.Message);
-		}
+            return new OkObjectResult(results);
+        }
+        catch (Exception ex)
+        {
+            log.LogError(ex.Message);
+            return new BadRequestObjectResult(ex.Message);
+        }
 
-	}
+    }
 
 
-	//mqtt
-	static IMqttClient mqttClient = null;
+    //mqtt
+    static IMqttClient mqttClient = null;
 
-	[FunctionName("CheckConnection")]
-	public static void CheckConnection([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer, ILogger log)
-	{
-		BrokerInfo broker = new BrokerInfo() { BrokerAddress = "13.81.105.139", BrokerPort = 1883 };
+    [FunctionName("CheckConnection")]
+    public static void CheckConnection([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer, ILogger log)
+    {
+        BrokerInfo broker = new BrokerInfo() { BrokerAddress = "13.81.105.139", BrokerPort = 1883 };
 
-		if (mqttClient == null) Connect(broker);
-		else if (!mqttClient.IsConnected) Connect(broker);
-	}
+        if (mqttClient == null) Connect(broker);
+        else if (!mqttClient.IsConnected) Connect(broker);
+    }
 
-	public static async void Connect(BrokerInfo client)
-	{
+    public static async void Connect(BrokerInfo client)
+    {
 
-		var factory = new MqttFactory();
-		mqttClient = factory.CreateMqttClient();
-		MqttClientOptions options = new MqttClientOptionsBuilder()
-			.WithClientId(client.ClientId)
-			.WithTcpServer(client.BrokerAddress, client.BrokerPort)
-			.WithCleanSession()
-			.Build();
+        var factory = new MqttFactory();
+        mqttClient = factory.CreateMqttClient();
+        MqttClientOptions options = new MqttClientOptionsBuilder()
+            .WithClientId(client.ClientId)
+            .WithTcpServer(client.BrokerAddress, client.BrokerPort)
+            .WithCleanSession()
+            .Build();
 
-		await mqttClient.ConnectAsync(options);
-		while (!mqttClient.IsConnected)
-		{
-			await Task.Delay(1000);
-			Console.WriteLine("Connecting to MQTT Broker ...");
-		}
+        await mqttClient.ConnectAsync(options);
+        while (!mqttClient.IsConnected)
+        {
+            await Task.Delay(1000);
+            Console.WriteLine("Connecting to MQTT Broker ...");
+        }
 
-		Subscription subscription = new Subscription();
-		List<string> topics = subscription.Topics;
-		foreach (string topic in topics)
-		{
-			SubscribeTopicAsync(mqttClient, topic);
-		}
-		HandleMessages();
-	}
+        Subscription subscription = new Subscription();
+        List<string> topics = subscription.Topics;
+        foreach (string topic in topics)
+        {
+            SubscribeTopicAsync(mqttClient, topic);
+        }
+        HandleMessages();
+    }
 
-	public static async Task PublishMessageAsync(IMqttClient mqttClient, string message)
-	{
-		var mqttMessage = new MqttApplicationMessageBuilder()
-			.WithTopic("hetJachtSeizoen/gameResults")
-			.WithPayload(message)
-			.WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.ExactlyOnce)
-			.WithRetainFlag()
-			.Build();
+    public static async Task PublishMessageAsync(IMqttClient mqttClient, string message)
+    {
+        var mqttMessage = new MqttApplicationMessageBuilder()
+            .WithTopic("hetJachtSeizoen/gameResults")
+            .WithPayload(message)
+            .WithQualityOfServiceLevel(MQTTnet.Protocol.MqttQualityOfServiceLevel.ExactlyOnce)
+            .WithRetainFlag()
+            .Build();
 
-		if (mqttClient.IsConnected)
-		{
-			await mqttClient.PublishAsync(mqttMessage);
-			Console.WriteLine("Message Published");
-		}
-		else
-		{
-			Console.WriteLine("Client not connected");
-		}
-	}
+        if (mqttClient.IsConnected)
+        {
+            await mqttClient.PublishAsync(mqttMessage);
+            Console.WriteLine("Message Published");
+        }
+        else
+        {
+            Console.WriteLine("Client not connected");
+        }
+    }
 
-	public static async Task SubscribeTopicAsync(IMqttClient mqttClient, string topic)
-	{
-		var test = mqttClient.SubscribeAsync(topic);
-		Console.WriteLine("Subscribed to topic: " + topic);
-		Task.WaitAll(test);
-	}
+    public static async Task SubscribeTopicAsync(IMqttClient mqttClient, string topic)
+    {
+        var test = mqttClient.SubscribeAsync(topic);
+        Console.WriteLine("Subscribed to topic: " + topic);
+        Task.WaitAll(test);
+    }
 
-	public static async Task HandleMessages()
-	{
-		mqttClient.ApplicationMessageReceivedAsync += (e) =>
-				{
-					Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
-					var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
-					var topic = e.ApplicationMessage.Topic;
-					if (topic == "hetJachtSeizoen/GameResults")
-					{
-						Console.WriteLine("Game results received");
-						HandleGameResults(payload);
-					}
-					else if (topic == "hetJachtSeizoen/GameInfo")
-					{
-						Console.WriteLine("Game info received");
-						HandleGameInfo(payload);
-					}
-					return Task.CompletedTask;
-				};
-	}
+    public static async Task HandleMessages()
+    {
+        mqttClient.ApplicationMessageReceivedAsync += (e) =>
+                {
+                    Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                    var payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                    var topic = e.ApplicationMessage.Topic;
+                    if (topic == "hetJachtSeizoen/GameResults")
+                    {
+                        Console.WriteLine("Game results received");
+                        HandleGameResults(payload);
+                    }
+                    else if (topic == "hetJachtSeizoen/GameInfo")
+                    {
+                        Console.WriteLine("Game info received");
+                        HandleGameInfo(payload);
+                    }
+                    return Task.CompletedTask;
+                };
+    }
 
-	public static void HandleGameResults(string payload)
-	{
-		GameResults gameResults = JsonConvert.DeserializeObject<GameResults>(payload);
-		Console.WriteLine($"Game results: \n\t" + gameResults.GameId + $"\n\t" + gameResults.Winner);
-		//send to database (datenow, winner, GameId)
-	}
+    public static void HandleGameResults(string payload)
+    {
+        GameResults gameResults = JsonConvert.DeserializeObject<GameResults>(payload);
+        Console.WriteLine($"Game results: \n\t" + gameResults.GameId + $"\n\t" + gameResults.Winner);
+        //send to database (datenow, winner, GameId)
+    }
 
-	public static void HandleGameInfo(string payload)
-	{
-		GameInfo gameInfo = JsonConvert.DeserializeObject<GameInfo>(payload);
-		Console.WriteLine("Game info: " + gameInfo);
-		//send to database if game is over
-	}
+    public static void HandleGameInfo(string payload)
+    {
+        GameInfo gameInfo = JsonConvert.DeserializeObject<GameInfo>(payload);
+        Console.WriteLine("Game info: " + gameInfo);
+        //send to database if game is over
+    }
 }
 
 
--- Models/Game.cs
+++ Models/Game.cs
@@ -2,48 +2,46 @@ namespace MCT.Functions.Models;
 
 public class Game
 {
-	private long _TimeLimit = 0;
+    private long _TimeLimit = 0;
 
-	[JsonProperty("groep")]
-	public string Groep { get; set; }
+    [JsonProperty("groep")]
+    public string Groep { get; set; }
 
-	[JsonProperty("BoefLatitude")]
-	public float Latitude { get; set; }
+    [JsonProperty("BoefLatitude")]
+    public float Latitude { get; set; }
 
-	[JsonProperty("BoefLongtitude")]
-	public float Longtitude { get; set; }
+    [JsonProperty("BoefLongtitude")]
+    public float Longtitude { get; set; }
 
-	[JsonProperty("spelcode")]
-	public string GameId { get; set; }
+    [JsonProperty("spelcode")]
+    public string GameId { get; set; }
 
-	[JsonProperty("inProgress")]
-	public bool GameInProgress { get; set; }
+    [JsonProperty("inProgress")]
+    public bool GameInProgress { get; set; }
 
-	[JsonProperty("timeLimit")]
-	public long TimeLimit
-	{
-		get { return _TimeLimit; }
+    [JsonProperty("timeLimit")]
+    public long TimeLimit
+    {
+        get { return _TimeLimit; }
 
-		set
-		{
-			List<long> timeLimits = new List<long> { 15 * 60, 60 * 60, 120 * 60, 240 * 60 };
-			if (timeLimits.Contains(value)) _TimeLimit = value;
-		}
-	}
+        set
+        {
+            List<long> timeLimits = new List<long> { 15 * 60, 60 * 60, 120 * 60, 240 * 60 };
+            if (timeLimits.Contains(value)) _TimeLimit = value;
+        }
+    }
 
-	[JsonProperty("startTime")]
-	public DateTime StartTime { get; set; }
+    [JsonProperty("startTime")]
+    public DateTime StartTime { get; set; }
 
-	[JsonProperty("endTime")]
-	public DateTime EndTime { get; set; }
+    [JsonProperty("endTime")]
+    public DateTime EndTime { get; set; }
 
-	[JsonProperty("aantalSpelers")]
-	public int AantalSpelers { get; set; }
+    [JsonProperty("aantalSpelers")]
+    public int AantalSpelers { get; set; }
 
-
-
-	[JsonProperty("id")]
-	public string Id { get; set; }
+    [JsonProperty("id")]
+    public string Id { get; set; }
 
 
 }

371c3d0d4adc34a559fe7a96ec34a037052f9f42
BlommePieter <pieter.blomme@student.howest.be>
2023-01-26 13:41:00 +0100

    game variables time voor js

--- Models/Game.cs
+++ Models/Game.cs
@@ -4,48 +4,54 @@ public class Game
 {
 
 
-	[JsonProperty("groep")]
-	public string Groep { get; set; }
+    [JsonProperty("groep")]
+    public string Groep { get; set; }
 
-	[JsonProperty("BoefLatitude")]
-	public float Latitude { get; set; }
+    [JsonProperty("BoefLatitude")]
+    public float Latitude { get; set; }
 
-	[JsonProperty("BoefLongtitude")]
-	public float Longtitude { get; set; }
+    [JsonProperty("BoefLongtitude")]
+    public float Longtitude { get; set; }
 
-	[JsonProperty("spelcode")]
-	public string GameId { get; set; }
+    [JsonProperty("spelcode")]
+    public string GameId { get; set; }
 
-	[JsonProperty("inProgress")]
-	public bool GameInProgress { get; set; }
+    [JsonProperty("inProgress")]
+    public bool GameInProgress { get; set; }
 
-	[JsonProperty("startTime")]
-	public DateTime StartTime { get; set; }
+    [JsonProperty("startTime")]
+    public DateTime StartTime { get; set; }
 
-	[JsonProperty("endTime")]
-	public DateTime EndTime { get; set; }
+    [JsonProperty("endTime")]
+    public DateTime EndTime { get; set; }
 
-	[JsonProperty("aantalSpelers")]
-	public int AantalSpelers { get; set; }
+    [JsonProperty("startTimeJs")]
+    public long StartTimeJs { get; set; }
 
-	[JsonProperty("jagersWinnen")]
-	public bool Winner { get; set; }
+    [JsonProperty("endTimeJs")]
+    public long EndTimeJs { get; set; }
 
-	[JsonProperty("startSpelkeuze")]
-	public bool startSpelkeuze { get; set; }
+    [JsonProperty("aantalSpelers")]
+    public int AantalSpelers { get; set; }
 
-	[JsonProperty("startSpel")]
-	public bool startSpel { get; set; }
+    [JsonProperty("jagersWinnen")]
+    public bool Winner { get; set; }
 
-	[JsonProperty("durationGame")]
-	public int durationGame { get; set; }
+    [JsonProperty("startSpelkeuze")]
+    public bool startSpelkeuze { get; set; }
 
-	[JsonProperty("durationLocation")]
-	public int durationLocation { get; set; }
+    [JsonProperty("startSpel")]
+    public bool startSpel { get; set; }
 
-	[JsonProperty("beginJager")]
-	public bool beginJager { get; set; }
+    [JsonProperty("durationGame")]
+    public int durationGame { get; set; }
 
-	[JsonProperty("id")]
-	public string Id { get; set; }
+    [JsonProperty("durationLocation")]
+    public int durationLocation { get; set; }
+
+    [JsonProperty("beginJager")]
+    public bool beginJager { get; set; }
+
+    [JsonProperty("id")]
+    public string Id { get; set; }
 }
